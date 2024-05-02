52e16c90ccc947d23e00a48c976d71f9a4356b15
marcojoao <marcojoao11@gmail.com>
2024-05-02 15:58:50 +0100

    update docker-compose

--- lib/env.py
+++ lib/env.py
@@ -6,12 +6,12 @@ APP_PORT: int = int(os.getenv("APP_PORT") or 8000)
 APP_LOG_LEVEL: str = os.getenv("APP_LOG_LEVEL") or "info"
 APP_TIMEOUT: int = int(os.getenv("APP_TIMEOUT") or 600)
 
-TMDB_API_KEY: str | None = os.getenv("TMDB_API_KEY")
+TMDB_API_KEY: str | None = os.getenv("TMDB_API_KEY") or None
 
-TRAKT_CLIENT_ID: str | None = os.getenv("TRAKT_CLIENT_ID")
-TRAKT_CLIENT_SECRET: str | None = os.getenv("TRAKT_CLIENT_SECRET")
+TRAKT_CLIENT_ID: str | None = os.getenv("TRAKT_CLIENT_ID") or None
+TRAKT_CLIENT_SECRET: str | None = os.getenv("TRAKT_CLIENT_SECRET") or None
 
-FIREBASE_PROD_KEY: str | None = os.getenv("FIREBASE_PROD_KEY")
-FIREBASE_STAGING_KEY: str | None = os.getenv("FIREBASE_STAGING_KEY")
+FIREBASE_PROD_KEY: str | None = os.getenv("FIREBASE_PROD_KEY") or None
+FIREBASE_STAGING_KEY: str | None = os.getenv("FIREBASE_STAGING_KEY") or None
 
 SPONSOR: str = os.getenv("SPONSOR") or ""
